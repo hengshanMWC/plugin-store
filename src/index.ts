1a829681f8c5f738888e541aff4ac16be7c013f6
马文灿 <mawencan@cvte.com>
2023-05-04 14:15:51 +0800

    feat: 重做了方法

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,7 @@ type CreatePluginData<T> = () => T
 
 type AddType<T> = T | CreatePluginData<T>
 export class PluginStore<T extends { id: any }> {
-  map: Map<string, T> = new Map()
-
-  add(plugin: AddType<T>) {
-    const pluginData: T = typeof plugin === 'function' ? plugin() : plugin
-    if (this.map.has(pluginData.id)) {
-      console.log('There are duplicate plugin ID')
-    }
-    else {
-      this.map.set(pluginData.id, pluginData)
-    }
-    return this
-  }
-
-  async readAdd(route: string) {
-    const plugin = await import(route)
-    return this.add(plugin.default)
-  }
+  private map: Map<string, T> = new Map()
 
   async use(...plugins: Array< AddType<T> | string>) {
     for (let i = 0; i < plugins.length; i++) {
@@ -32,7 +16,27 @@ export class PluginStore<T extends { id: any }> {
     }
   }
 
+  getPlugin(id: T['id']) {
+    return this.map.get(id)
+  }
+
   remove(id: string) {
     this.map.delete(id)
+  }
+
+  private add(plugin: AddType<T>) {
+    const pluginData: T = typeof plugin === 'function' ? plugin() : plugin
+    if (this.map.has(pluginData.id)) {
+      console.log('There are duplicate plugin ID')
+    }
+    else {
+      this.map.set(pluginData.id, pluginData)
+    }
+    return this
+  }
+
+  private async readAdd(route: string) {
+    const plugin = await import(route)
+    return this.add(plugin.default)
   }
 }
--- test/specs/index.test.ts
+++ test/specs/index.test.ts
@@ -2,36 +2,26 @@ import { resolve } from 'node:path'
 import { PluginStore } from '../../src/index'
 import createPluginDefault, { createTestPlugin } from '../__fixtures__/plugin'
 
-const createPluginDefaultPath = resolve(__dirname, '../__fixtures__/plugin.ts')
 describe('plugin', () => {
-  test('default', () => {
-    const pluginStore = new PluginStore()
-    pluginStore.add(createTestPlugin)
-    expect(pluginStore.map.get('test')).toMatchObject(createTestPlugin())
+  const pluginTestId = createTestPlugin().id
+  const createPluginDefaultPath = resolve(__dirname, '../__fixtures__/plugin.ts')
+  test('use', async () => {
+    const pluginStore = new PluginStore<{ id: string }>()
+    await pluginStore.use(createPluginDefaultPath, createTestPlugin)
+    expect(pluginStore.getPlugin(createPluginDefault().id)).toMatchObject(createPluginDefault())
+    expect(pluginStore.getPlugin(pluginTestId)).toMatchObject(createTestPlugin())
   })
   test('repeat', () => {
     const pluginStore = new PluginStore()
     const plugin = createTestPlugin()
-    pluginStore.add(plugin)
-    pluginStore.add(createTestPlugin)
-    expect(pluginStore.map.get('test')).toBe(plugin)
+    pluginStore.use(plugin, createTestPlugin)
+    expect(pluginStore.getPlugin(pluginTestId)).toBe(plugin)
   })
   test('remove', () => {
     const pluginStore = new PluginStore()
-    pluginStore.add(createTestPlugin)
-    expect(pluginStore.map.get('test')).not.toBeUndefined()
-    pluginStore.remove('test')
-    expect(pluginStore.map.get('test')).toBeUndefined()
-  })
-  test('readAdd', async () => {
-    const pluginStore = new PluginStore()
-    await pluginStore.readAdd(createPluginDefaultPath)
-    expect(pluginStore.map.get('default')).toMatchObject(createPluginDefault())
-  })
-  test('use', async () => {
-    const pluginStore = new PluginStore<{ id: string }>()
-    await pluginStore.use(createPluginDefaultPath, createTestPlugin)
-    expect(pluginStore.map.get('default')).toMatchObject(createPluginDefault())
-    expect(pluginStore.map.get('test')).toMatchObject(createTestPlugin())
+    pluginStore.use(createTestPlugin)
+    expect(pluginStore.getPlugin(pluginTestId)).not.toBeUndefined()
+    pluginStore.remove(pluginTestId)
+    expect(pluginStore.getPlugin(pluginTestId)).toBeUndefined()
   })
 })
